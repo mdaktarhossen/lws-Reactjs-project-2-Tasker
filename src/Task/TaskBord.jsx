b86e035f8144be72162b75e353606243186d3664
mdaktarhossen <mdaktarhossen20@gmail.com>
2024-01-27 12:47:49 +0600

    project done wow, feeling so good

--- src/Task/TaskBord.jsx
+++ src/Task/TaskBord.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import AddTaskModal from "./AddTaskModal";
+import NoTaskFound from "./NoTaskFound";
 import SearchTask from "./SearchTask";
 import TastAction from "./TastAction";
 import TastLists from "./TastLists";
@@ -79,7 +80,7 @@ export default function TaskBord() {
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TastAction onAddTask={() => setAddTaskModal(true)} onDeleteAllTasks={handleOnDeleteAllTasks} />
-          <TastLists tasks={tasks} onEdit={handleEditTask} onDelete={handleDeleteTask} isFav={handleIsFav} />
+          {tasks.length > 0 ? <TastLists tasks={tasks} onEdit={handleEditTask} onDelete={handleDeleteTask} isFav={handleIsFav} /> : <NoTaskFound />}
         </div>
       </div>
     </section>
